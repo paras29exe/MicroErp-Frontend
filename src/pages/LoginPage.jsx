c65fd516ed08d6a54e43764e0ed8cb083a79502c
Paras <Paras.webdev404@gmail.com>
2026-05-08 14:18:31 +0530

    Replace Link with div for forgot password section

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -97,12 +97,12 @@ export function LoginPage() {
                 <p className="text-xs text-red-700">{errors.password.message}</p>
               )}
               <div className="text-right pt-1">
-                <Link
+                <div
                   to="/forgot-password"
                   className="text-xs text-blue-700 hover:underline"
                 >
                   Forgot password?
-                </Link>
+                </div>
               </div>
             </div>
 
